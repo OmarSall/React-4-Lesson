1ba3a1d15ad3b1dbe586517c8c3f912bf4cd5e49
Omar Salloum <omarsalloum08@gmail.com>
2025-05-21 14:11:17 +0200

    second exercise lesson

--- useMemo Lesson/src/App.jsx
+++ useMemo Lesson/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import "./App.css"
 import TaskFilterList from "./components/TaskFilterList";
+import PostsList from "./components/PostsList";
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
     <div className="card">
       <h1>My Tasks</h1>
       <TaskFilterList />
-
+      <PostsList limit={5} userId={1}/>
     </div>
   )
 }
---
+++ useMemo Lesson/src/components/PostsList.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useMemo, useState } from "react";
+
+export default function PostsList({ limit= 10, userId }) {
+    const [posts, setPosts] = useState([]);
+
+    const configuration = useMemo(() => {
+        const config = {
+            _limit: limit,
+            _sort: "title",
+        };
+        if (userId) {
+            config.userId = userId;
+        }
+        return config;
+    }, [limit, userId]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const searchParams = new URLSearchParams(configuration);
+                const response = await fetch(`https://jsonplaceholder.typicode.com/posts?${searchParams.toString()}`);
+                const newPost = await response.json();
+                setPosts(newPost);
+            } catch (error) {
+                console.error("Error fetching posts:", error);
+            }
+        }
+
+        fetchPosts();
+    }, [configuration]);
+
+    return (
+        <div>
+            {posts.map(({ title, body, id }) => (
+                <div key={id}>
+                    <h2>{title}</h2>
+                    <p>{body}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
